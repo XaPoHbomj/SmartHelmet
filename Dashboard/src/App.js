e93533425ee90af4c1571441d9f7aae014e61b0e
Никита Великий <mtyoungstz@outlook.com>
2022-05-22 04:12:30 +0500

    Теперь Dashboard отображается корректно. Исправлена парочка мелких багов. Произведен рефакторинг кода

--- Dashboard/src/App.js
+++ Dashboard/src/App.js
@@ -2,7 +2,7 @@ import HubListener from "./components/signalr/HubListener";
 import SiteHeader from "./components/header/SiteHeader";
 import { CloudOutlined } from "@ant-design/icons";
 import HelmetOverview from "./components/overview/HelmetOverview";
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useState } from "react";
 import HelmetPreview from "./components/overview/helmet/HelmetPreview";
 import SignalLevel from "./components/overview/helmet/indicators/SignalLevel";
 import BatteryLevel from "./components/overview/helmet/indicators/BatteryLevel";
@@ -15,125 +15,163 @@ import useStateRef from 'react-usestateref'
 export default function App() {
   const [canShowOverviewSkeleton, showOverviewSkeleton] = useState(true);
   const [isDashboardOpen, showDashboard, isDashboardOpenRef] = useStateRef(false);
-  const [dashboardTarget, updateDashboardTarget, dashboardTargetRef] = useStateRef({});
-  const [helmets, updateHelmets] = useState([]);
+  
+  const [
+    helmets, 
+    updateHelmets, 
+    helmetsRef
+  ] = useStateRef([]);
+  
+  const [
+    dashboardValues, 
+    updateDashboardValues, 
+    dashboardValuesRef
+  ] = useStateRef([]);
+  
+  const [
+    dashboardTargetIdentificator, 
+    setDashboardTargetIdentificator, 
+    dashboardTargetIdentificatorRef
+  ] = useStateRef(null);
+
+  const updateOverview = (event) => {
+    const existingHelmets = [...helmetsRef.current];
+    const helmetIndex = existingHelmets.findIndex(
+      (helmet) => helmet.boardIdentificator === event.boardIdentificator
+    );
+
+    if (helmetIndex > -1) {
+      existingHelmets[helmetIndex] = event;
+      updateHelmets(existingHelmets);
+    }
+    else {
+      updateHelmets([
+        ...existingHelmets,
+        event
+      ]);
+    }
+  }
+  
+  const updateDashboard = (event) => {
+    const boardIdentificator = event.boardIdentificator;
+    const timestamp = moment(event.dateTime).format("HH:mm:ss");
+
+    const existingDashboardValues = [...dashboardValuesRef.current];
+    const dashboardValuesIndex = existingDashboardValues.findIndex(
+      (dashboardValues) => dashboardValues.identificator === boardIdentificator
+    );
+
+    const eventValues = {
+      timestamp,
+      smokeValue: event.smokeValue,
+      gyroscope: event.gyroscope
+    };
+
+    if (dashboardValuesIndex > -1) {
+      const boardValues = existingDashboardValues[dashboardValuesIndex].boardValues;
+      boardValues.push(eventValues);
+
+      if (boardValues.length > 15) {
+        boardValues.shift();
+      }
+    }
+    else {
+      existingDashboardValues.push({
+        identificator: boardIdentificator,
+        boardValues: [eventValues]
+      });
+    }
+    
+    updateDashboardValues(existingDashboardValues);
+
+    if (isDashboardOpenRef.current && dashboardTargetIdentificatorRef.current === boardIdentificator) {
+      notification.info({
+        key: "dashboardUpdated",
+        message: "Данные обновлены",
+        description: moment().format("DD.MM.yyyy г. в HH:mm:ss")
+      });
+    }
+  }
   
   const handleEvent = (event) => {
-    updateHelmets((previousState) => {
-      const existingHelmets = [...previousState];
-      const elementIndex = existingHelmets.findIndex(
-        (existingHelmet) => existingHelmet.boardIdentificator === event.boardIdentificator
-      );
-
-      if (elementIndex > -1) {
-        existingHelmets[elementIndex] = {
-		  ...event,
-		  smokeValues: [
-			...existingHelmets[elementIndex].smokeValues,
-			{
-			  timestamp: event.dateTime,
-			  value: event.smokeValue
-			}
-		  ]
-		};
-		
-        return existingHelmets;
-      }
-	  
-	  const helmet = {
-		...event,
-		smokeValues: []
-	  };
-
-      return [
-        ...existingHelmets,
-		helmet
-      ];
-    });
-	
-	if (isDashboardOpenRef.current && dashboardTargetRef.current.boardIdentificator === event.boardIdentificator) {
-	  notification.info({
-		message: "Данные обновлены",
-		description: moment().format("DD.MM.yyyy г. в HH:mm:ss")
-	  });
-
-	  updateDashboardTarget(event);
-	}
+	updateOverview(event);
+    updateDashboard(event);
   };
-  
+
   const hubCallbacks = {
-	onStarted: () => {
-	  showOverviewSkeleton(false);
-	},
-	onEventReceived: (event) => handleEvent({
-	  ...event,
-	  isOnline: true
-	})
+    onStarted: () => {
+      showOverviewSkeleton(false);
+    },
+    onEventReceived: (event) => handleEvent({
+      ...event,
+      isOnline: true
+    })
   };
 
   const onHelmetRemove = (identificator) => {
-	updateHelmets(
-	  helmets.filter(
-		(existingHelmet) => existingHelmet.boardIdentificator !== identificator
-	  )
-	);
+    updateHelmets(
+      helmets.filter(
+        (existingHelmet) => existingHelmet.boardIdentificator !== identificator
+      )
+    );
   };
 
   const onDashboardOpen = (identificator) => {
-	const helmet = helmets.find(
-	  (existingHelmet) => existingHelmet.boardIdentificator === identificator
-	);
+    const helmet = helmets.find(
+      (existingHelmet) => existingHelmet.boardIdentificator === identificator
+    );
 
-	updateDashboardTarget(helmet);
-	showDashboard(true);
+    setDashboardTargetIdentificator(helmet.boardIdentificator);
+    showDashboard(true);
   };
 
   const onDashboardClose = () => {
-	showDashboard(false);
+    showDashboard(false);
   };
 
   return (
-	<Fragment>
-	  <HubListener
-		endpoint="https://localhost:7217/board/"
-		target="getUpdates"
-		callbacks={hubCallbacks}
-	  />
-	  <SiteHeader
-		header="SmartHelmet"
-		icon={<CloudOutlined/>}
-	  />
-	  <HelmetOverview
-		showSkeleton={canShowOverviewSkeleton}
-		skeletonSettings={{
-		  helmetsCount: 6,
-		  active: true,
-		  size: "small"
-		}}
-	  >
-		{helmets.map((helmet) => (
-		  <HelmetPreview
-			key={helmet.boardIdentificator}
-			identificator={helmet.boardIdentificator}
-			isOnline={helmet.isOnline}
-			charging={helmet.charging}
-			isDismounted={helmet.isDismounted}
-			isFellOff={helmet.isFellOff}
-			isHighSmokeLevel={helmet.isHighSmokeLevel}
-			onHelmetRemove={onHelmetRemove}
-			onDashboardOpen={onDashboardOpen}
-		  >
-			<SignalLevel value={helmet.signalLevel}/>
-			<BatteryLevel value={helmet.batteryLevel}/>
-			<Timestamp value={moment(helmet.dateTime).format("HH:mm:ss")}/>
-		  </HelmetPreview>
-		))}
-	  </HelmetOverview>
-	  <Dashboard
-		target={dashboardTarget}
-		isOpened={isDashboardOpen}
-		onClose={onDashboardClose}
-	  />
-	</Fragment>
+    <Fragment>
+      <HubListener
+        endpoint="https://localhost:7217/board/"
+        target="getUpdates"
+        callbacks={hubCallbacks}
+      />
+      <SiteHeader
+        header="SmartHelmet"
+        icon={<CloudOutlined/>}
+      />
+      <HelmetOverview
+        showSkeleton={canShowOverviewSkeleton}
+        skeletonSettings={{
+          helmetsCount: 6,
+          active: true,
+          size: "small"
+        }}
+      >
+        {helmets.map((helmet) => (
+          <HelmetPreview
+            key={helmet.boardIdentificator}
+            identificator={helmet.boardIdentificator}
+            isOnline={helmet.isOnline}
+            charging={helmet.charging}
+            isDismounted={helmet.isDismounted}
+            isFellOff={helmet.isFellOff}
+            isHighSmokeLevel={helmet.isHighSmokeLevel}
+            onHelmetRemove={onHelmetRemove}
+            onDashboardOpen={onDashboardOpen}
+          >
+            <SignalLevel value={helmet.signalLevel}/>
+            <BatteryLevel value={helmet.batteryLevel}/>
+            <Timestamp value={moment(helmet.dateTime).format("HH:mm")}/>
+          </HelmetPreview>
+        ))}
+      </HelmetOverview>
+      <Dashboard
+        targetIdentificator={dashboardTargetIdentificator}
+        values={dashboardValues}
+        isOpened={isDashboardOpen}
+        onClose={onDashboardClose}
+      />
+    </Fragment>
   );
 }
--- Dashboard/src/components/dashboard/Dashboard.js
+++ Dashboard/src/components/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-﻿import { Button } from "antd";
+﻿import { Button, Empty } from "antd";
 import Modal from "antd/lib/modal/Modal";
 import {
   Chart as ChartJS,
@@ -11,8 +11,6 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { useEffect, useState } from "react";
-import moment from "moment";
 
 ChartJS.register(
   CategoryScale,
@@ -25,57 +23,67 @@ ChartJS.register(
 );
 
 export default function Dashboard(props) {
-  const [smokeValues, updateSmokeValues] = useState([]);
-  
-  useEffect(() => {
-    console.log('props changed')
-    if (props.target) {
-      updateSmokeValues((previousState) => [
-        ...previousState,
-        {
-          label: moment(props.target.dateTime).format("HH:mm:ss"),
-          value: props.target.smokeValue
-        }
-      ]);
-    }
-  }, [props.target]);
-
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top',
+        display: false,
+        position: 'top'
       },
       title: {
         display: true,
         text: "Датчик дыма",
-      },
-    },
+      }
+    }
   };
   
-  const chart = {
-    labels: smokeValues.map(smokeValue => smokeValue.label),
-    datasets: [{
-      label: "Уровень задымления",
-      data: smokeValues.map(smokeValue => smokeValue.value)
-    }]
-  };
+  const renderModal = (body) => {
+    return (
+      <Modal
+        title="Статистика"
+        visible={props.isOpened}
+        onCancel={props.onClose}
+        footer={[
+          <Button key="close" onClick={props.onClose}>
+            Закрыть
+          </Button>
+        ]}
+      >
+        {body}
+      </Modal>
+    );
+  }
   
-  return (
-    <Modal
-      title="Статистика"
-      visible={props.isOpened}
-      onCancel={props.onClose}
-      footer={[
-        <Button key="close" onClick={props.onClose}>
-          Закрыть
-        </Button>
-      ]}
-    >
-     <Line 
-       data={chart} 
-       options={options} 
-     /> 
-    </Modal>
+  if (props.targetIdentificator) {
+    const dashboardValues = props.values.find(
+      (targetValues) => targetValues.identificator === props.targetIdentificator
+    );
+    
+    if (dashboardValues) {
+      const chartData = {
+        labels: dashboardValues.boardValues.map(boardValue => boardValue.timestamp),
+        datasets: [{
+          label: "Уровень задымления",
+          data: dashboardValues.boardValues.map(boardValue => boardValue.smokeValue),
+          borderColor: 'rgb(255, 99, 132, 0.7)',
+          backgroundColor: 'rgba(255, 99, 132, 0.7)',
+          borderWidth: 2
+        }]
+      };
+
+      return renderModal(
+        <Line
+          data={chartData}
+          options={options}
+        />
+      );
+    }
+  }
+  
+  return renderModal(
+    <Empty
+      description="Статистика не найдена"
+      image={Empty.PRESENTED_IMAGE_SIMPLE}
+    />
   );
 }
